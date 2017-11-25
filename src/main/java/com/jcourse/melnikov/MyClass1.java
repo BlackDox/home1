49c2a8ec01317dd07a4e66ba3bd5ca93e8580f71
BlackDox <black_dox@mail.ru>
2017-11-25 21:52:22 +0700

    Test changes

--- src/main/java/com/jcourse/melnikov/MyClass1.java
+++ src/main/java/com/jcourse/melnikov/MyClass1.java
@@ -1,8 +1,6 @@
 package com.jcourse.melnikov;
 
-/**
- * Created by BlackDox on 25.11.2017.
- */
+
 public class MyClass1 {
     public static void main(String[] args) {
         System.out.println("Первая задача");
