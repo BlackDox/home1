e9eef890cdb0b874c64da3ee359e558d874db5e2
BlackDox <black_dox@mail.ru>
2017-11-28 16:25:47 +0700

    Test changes

---
+++ src/main/java/com/jcourse/melnikov/Messages.java
@@ -0,0 +1,14 @@
+package com.jcourse.melnikov;
+
+/**
+ * Created by BlackDox on 28.11.2017.
+ */
+public class Messages {
+    String mesHello = "Угадай число от 1 до 100!";
+    String mesError = "Ошибка!\n***Ты должен вводить целые числа от 1 до 100!***";
+    String mesWin = "Вы угадали!!!";
+    String mesMore = "Загаданное число больше!";
+    String mesLess = "Загаданное число меньше!";
+    String mesLose = "Вы проиграли!!!\nЗагаданное число = ";
+    String mesTryIt = "Всего попыток 8! Введи число! Это ваша попытка № ";
+}
--- src/main/java/com/jcourse/melnikov/MyClass1.java
+++ src/main/java/com/jcourse/melnikov/MyClass1.java
@@ -1,8 +1,46 @@
 package com.jcourse.melnikov;
 
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MyClass1 {
+    static Messages messages = new Messages();
+    static boolean isWin = false;
+
     public static void main(String[] args) {
-        System.out.println("Первая задача");
+        int number = (int) (1 + Math.random()*99); //Генерация угадываемого числа
+        System.out.println(messages.mesHello);
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            int attempt = 1;
+            while (!isWin && (attempt <= 8)) {
+                System.out.println(messages.mesTryIt+attempt);
+                int myNumber = Integer.parseInt(reader.readLine());
+                guessingGame(number, myNumber);
+                attempt++;
+            }
+            reader.close();
+        } catch (NumberFormatException e) {
+            System.out.println(messages.mesError);
+        } catch (IOException e) {
+        }
+
+        if (!isWin) {
+            System.out.println(messages.mesLose + number);
+        }
+    }
+
+    static void guessingGame(int number, int myNumber){
+        if (number == myNumber) {
+            System.out.println(messages.mesWin);
+            isWin = true;
+        }
+        if (number > myNumber) { System.out.println(messages.mesMore); }
+        if (number < myNumber) { System.out.println(messages.mesLess); }
     }
 }
